d87e31c9682d3f56144951dcf0b8a7dc39ec5e1f
Michael Liston <michaelowen.liston@gmail.com>
2016-03-20 15:00:44 -0400

    Back in business

---
+++ myfontscript.js
@@ -0,0 +1,12 @@
+<script type="text/javascript">
+    (function() {
+        var path = '//easy.myfonts.net/v2/js?sid=269724(font-family=Americane+Light)&sid=269725(font-family=Americane+Medium)&sid=269726(font-family=Americane+Regular)&sid=269732(font-family=Americane+Bold)&key=ZypiqWWxUk',
+            protocol = ('https:' == document.location.protocol ? 'https:' : 'http:'),
+            trial = document.createElement('script');
+        trial.type = 'text/javascript';
+        trial.async = true;
+        trial.src = protocol + path;
+        var head = document.getElementsByTagName("head")[0];
+        head.appendChild(trial);
+    })();
+</script>
